5dfa2950a7bb17bec439a4003eb8ba50be73106b
Daniel Ayuko <dan.ayuko@gmail.com>
2025-12-31 19:44:13 +0300

    added a JavaScript file and modified index.html

---
+++ practice.js
@@ -0,0 +1,39 @@
+// Functions that parse and utilize parameters
+let userName = "Mayajane Doro";
+
+function greetUser(userName){
+    console.log("Hello, " + userName + "!");
+}
+ 
+greetUser(userName);
+
+// Function that returns a value
+
+function calculateArea(length, width){
+    return length * width;
+}
+
+let length = 5;
+let width = 10;
+let area = calculateArea(length, width  );
+console.log("Area: " + area);   
+
+// Function with default parameters
+
+function introduce(name, age = 25){
+    console.log("My name is " + name + " and I am " + age + " years old.");
+}       
+
+introduce("Alice");
+introduce("Bob", 25); 
+
+
+
+//annonymous function assigned to a variable
+let square = function(number){
+    return number * number;
+};  
+
+setTimeout(function(){
+    console.log("Square of 4 is: " + square(4));
+}, 8000);  
