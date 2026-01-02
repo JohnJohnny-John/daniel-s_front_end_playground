2793253be166ee8dac00038556ee7d0b031a8fa9
Daniel Ayuko <dan.ayuko@gmail.com>
2026-01-02 17:53:15 +0300

    modifications of the index.html, style.cc and practice.js files

--- practice.js
+++ practice.js
@@ -37,3 +37,27 @@ let square = function(number){
 setTimeout(function(){
     console.log("Square of 4 is: " + square(4));
 }, 8000);  
+
+// array methods    
+array.forEach(element => {
+    console.log(element);       
+});
+
+let array = [1, 2, 3, 4, 5];
+
+let doubledArray = array.map(function(element){
+    return element * 2;
+}); 
+
+// Creating a function
+function sayHello(){
+    console.log("Hello, World!");
+}
+
+// Calling the function
+sayHello();
+
+// Function using arrow syntax
+() => {
+    console.log("Hello, World!");
+}
